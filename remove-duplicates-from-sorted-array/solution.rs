7ebb91430d26414702a84c10f7d6c8ddd82c4abe
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2024-08-28 14:13:55 -0300

    fix indentation

--- remove-duplicates-from-sorted-array/solution.rs
+++ remove-duplicates-from-sorted-array/solution.rs
@@ -1,15 +1,15 @@
 fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-	if nums.is_empty() {
-		return 0;
-	}
-	let mut k: i32 = 1;
-	for i in 1..nums.len() {
-		if nums[i] != nums[i - 1] {
-			nums[k as usize] = nums[i];
-			k += 1;
-		}
-	}
-	k
+  if nums.is_empty() {
+    return 0;
+  }
+  let mut k: i32 = 1;
+  for i in 1..nums.len() {
+    if nums[i] != nums[i - 1] {
+      nums[k as usize] = nums[i];
+      k += 1;
+    }
+  }
+  k
 }
 
 fn main() {
